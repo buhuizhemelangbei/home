93d94e6d6abd81e095e04f2bc6332b62a8d3dfda
buhuizhemelangbei <153802970+buhuizhemelangbei@users.noreply.github.com>
2024-02-07 21:01:38 +0800

    Update Footer.vue

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -14,8 +14,8 @@
         <!-- 以下信息请不要修改哦 -->
         <span class="hidden">
           &amp;&nbsp;Made&nbsp;by
-          <a :href="config.github" target="_blank">
-            {{  }}
+          <a :href="zenhuizhemelangbei.love" target="_blank">
+          China
           </a>
         </span>
         <!-- 站点备案 -->
