3941da2fbb97c2f652643023fc210770c729d256
buhuizhemelangbei <153802970+buhuizhemelangbei@users.noreply.github.com>
2024-02-07 21:47:05 +0800

    Update GithubProject.vue

--- src/components/GithubProject.vue
+++ src/components/GithubProject.vue
@@ -2,7 +2,7 @@
   <div class="github-project">
     <div class="title">
       <github-one theme="two-tone" size="24" :fill="['#efefef', '#00000020']" />
-      <span>Github</span>
+      <span>一直欠一个抱歉@yjh</span>
     </div>
     <Swiper
       :modules="[Pagination, Mousewheel]"
